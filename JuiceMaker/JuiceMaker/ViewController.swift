12dbdfed6211b8437ce7422be9896eb527703a08
horeng2 <huski.a.a@gmail.com>
2022-05-16 23:56:37 +0900

    feat: 프로젝트 생성

---
+++ JuiceMaker/JuiceMaker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JuiceMaker
+//
+//  Created by 서녕 on 2022/05/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
