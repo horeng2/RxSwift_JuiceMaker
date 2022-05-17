28585640a79152d3f4eb27d503d24412b9cb11ab
horeng2 <huski.a.a@gmail.com>
2022-05-17 20:32:40 +0900

    feat: UI요소들을 ViewController에 연결

---
+++ JuiceMaker/JuiceMaker/OrderViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  JuiceMaker
+//
+//  Created by 서녕 on 2022/05/16.
+//
+
+import UIKit
+
+class OrderViewController: UIViewController {
+    @IBOutlet weak var strawberryStockLabel: UILabel!
+    @IBOutlet weak var bananaStockLabel: UILabel!
+    @IBOutlet weak var pineappleStockLabel: UILabel!
+    @IBOutlet weak var kiwiStockLabel: UILabel!
+    @IBOutlet weak var mangoStockLabel: UILabel!
+    
+    @IBOutlet weak var orderOfStrawberryBananaJuice: UIButton!
+    @IBOutlet weak var orderOfMangoKiwiJuice: UIButton!
+    @IBOutlet weak var orderOfStrawberryJuice: UIButton!
+    @IBOutlet weak var orderOfBananaJuice: UIButton!
+    @IBOutlet weak var orderOfPineappleJuice: UIButton!
+    @IBOutlet weak var orderOfKiwiJuice: UIButton!
+    @IBOutlet weak var orderOfMangoJuice: UIButton!
+  
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
--- JuiceMaker/JuiceMaker/OrderViewModel.swift
+++ JuiceMaker/JuiceMaker/OrderViewModel.swift
@@ -7,6 +7,6 @@
 
 import Foundation
 
-class OrderJuiceViewModel {
+class OrderViewModel {
     private let fruitStore = FruitStore.shared
 }
