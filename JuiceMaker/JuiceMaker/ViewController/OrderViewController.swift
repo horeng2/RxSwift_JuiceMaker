1d471a0dc86a7964d83f5e3c9836b3ca3440c4f6
horeng2 <huski.a.a@gmail.com>
2022-05-20 18:34:16 +0900

    feat: Output 선언 및 초기화면 구현

--- JuiceMaker/JuiceMaker/ViewController/OrderViewController.swift
+++ JuiceMaker/JuiceMaker/ViewController/OrderViewController.swift
@@ -24,29 +24,43 @@ class OrderViewController: UIViewController {
     @IBOutlet weak var orderOfKiwiJuice: UIButton!
     @IBOutlet weak var orderOfMangoJuice: UIButton!
     
-    var orderViewModel: OrderViewModel?
     var disposeBag = DisposeBag()
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        bindOrderButtons()
+        self.binding()
     }
     
-    private func bindOrderButtons() {
-        guard let orderViewModel = orderViewModel else {
-            return
-        }
+    private func binding() {
+        let orderViewModel = OrderViewModel()
 
         let input = OrderViewModel.Input(
-            orderStrawberryBananaButtonTap: orderOfStrawberryBananaJuice.rx.tap.asObservable(),
-            orderOfMangoKiwiButtonTap: orderOfMangoKiwiJuice.rx.tap.asObservable(),
-            orderOfStrawberryButtonTap: orderOfStrawberryJuice.rx.tap.asObservable(),
-            orderOfBananaButtonTap: orderOfBananaJuice.rx.tap.asObservable(),
-            orderOfPineappleButtonTap: orderOfPineappleJuice.rx.tap.asObservable(),
-            orderOfKiwiButtonTap: orderOfKiwiJuice.rx.tap.asObservable(),
-            orderOfMangoButtonTap: orderOfMangoJuice.rx.tap.asObservable()
+            viewWillAppear: self.rx.methodInvoked(#selector(UIViewController.viewWillAppear(_:))).map{ _ in }
+//            orderStrawberryBananaButtonTap: orderOfStrawberryBananaJuice.rx.tap.asObservable(),
+//            orderOfMangoKiwiButtonTap: orderOfMangoKiwiJuice.rx.tap.asObservable(),
+//            orderOfStrawberryButtonTap: orderOfStrawberryJuice.rx.tap.asObservable(),
+//            orderOfBananaButtonTap: orderOfBananaJuice.rx.tap.asObservable(),
+//            orderOfPineappleButtonTap: orderOfPineappleJuice.rx.tap.asObservable(),
+//            orderOfKiwiButtonTap: orderOfKiwiJuice.rx.tap.asObservable(),
+//            orderOfMangoButtonTap: orderOfMangoJuice.rx.tap.asObservable()
         )
+        
+        let output = orderViewModel.transform(input: input)
+        
+        output.currentStock
+            .drive(onNext: { stock in
+                self.strawberryStockLabel.text = String(stock[.strawberry] ?? 10)
+                self.bananaStockLabel.text = String(stock[.banana] ?? 10)
+                self.pineappleStockLabel.text = String(stock[.pineapple] ?? 10)
+                self.kiwiStockLabel.text = String(stock[.kiwi] ?? 10)
+                self.mangoStockLabel.text = String(stock[.mango] ?? 10)
+            })
+            .disposed(by: self.disposeBag)
+            
         
     }
 }
--- JuiceMaker/JuiceMaker/ViewModel/OrderViewModel.swift
+++ JuiceMaker/JuiceMaker/ViewModel/OrderViewModel.swift
@@ -11,34 +11,52 @@ import RxRelay
 import RxCocoa
 
 class OrderViewModel: ViewModelType {
-    private let fruitStore = FruitStore.shared
+    let fruitStock = BehaviorRelay<[Fruit: Int]>(value: FruitStore.shared.fruitStock)
+    
     let disposeBag = DisposeBag()
 
     struct Input {
-        var orderStrawberryBananaButtonTap: Observable<Void>
-        var orderOfMangoKiwiButtonTap: Observable<Void>
-        var orderOfStrawberryButtonTap: Observable<Void>
-        var orderOfBananaButtonTap: Observable<Void>
-        var orderOfPineappleButtonTap: Observable<Void>
-        var orderOfKiwiButtonTap: Observable<Void>
-        var orderOfMangoButtonTap: Observable<Void>
+        let viewWillAppear: Observable<Void>
+//        let orderStrawberryBananaButtonTap: Observable<Void>
+//        let orderOfMangoKiwiButtonTap: Observable<Void>
+//        let orderOfStrawberryButtonTap: Observable<Void>
+//        let orderOfBananaButtonTap: Observable<Void>
+//        let orderOfPineappleButtonTap: Observable<Void>
+//        let orderOfKiwiButtonTap: Observable<Void>
+//        let orderOfMangoButtonTap: Observable<Void>
     }
     
     struct Output {
-        var currentStock: Driver<[Fruit:Int]>
+        var currentStock: Driver<[Fruit: Int]>
+//        var bananaStock: Driver<Int>
+//        var pineappleStock: Driver<Int>
+//        var kiwiStock: Driver<Int>
+//        var mangoStock: Driver<Int>
     }
     
-//    func transform(input: Input) -> Output {
-//    }
-
-    func reload() {
+    func transform(input: Input) -> Output {
+        let currentStock = fruitStock.asDriver()
+        
+//        let bananaStock = fruitStock
+//            .map{ $0.filter{ $0.key == .banana } }
+//            .asDriver(onErrorJustReturn: [.banana: 0])
+//        let pineappleStock = fruitStock
+//            .map{ $0.filter{ $0.key == .pineapple } }
+//            .asDriver(onErrorJustReturn: [.pineapple: 0])
+//        let kiwiStock = fruitStock
+//            .map{ $0.filter{ $0.key == .kiwi } }
+//            .asDriver(onErrorJustReturn: [.kiwi: 0])
+//        let mangoStock = fruitStock
+//            .map{ $0.filter{ $0.key == .mango } }
+//            .asDriver(onErrorJustReturn: [.mango: 0])
+            
+            return Output(currentStock: currentStock)
     }
-
 }
 
 protocol ViewModelType: AnyObject {
     associatedtype Input
     associatedtype Output
     
-//    func transform(input: Input) -> Output
+    func transform(input: Input) -> Output
 }
