e36a12a041510da26ec9d1d29315d043de7d6a28
horeng2 <huski.a.a@gmail.com>
2022-05-23 16:02:16 +0900

    feat: FruitStore->FruitRepository 리네이밍, readStock 메소드 구현

---
+++ JuiceMaker/JuiceMaker/Repository/FruitRepository.swift
@@ -0,0 +1,41 @@
+//
+//  FruitStore.swift
+//  JuiceMaker
+//
+//  Created by 서녕 on 2022/05/17.
+//
+
+import Foundation
+import RxSwift
+import RxOptional
+
+class FruitRepository {
+    static let shared = FruitRepository()
+    var fruitStock = [Fruit : Int]()
+    private let initialStock = 10
+    
+    init() {
+        Fruit.allCases.forEach { fruit in
+            fruitStock.updateValue(self.initialStock, forKey: fruit)
+        }
+    }
+    
+    func readStock(of fruit: Fruit) -> Observable<Int> {
+        return Observable.just(self.fruitStock[fruit])
+                .filterNil()
+    }
+    
+    func increaseStock(of fruit: Fruit, count: Int) {
+        guard let currentStock = fruitStock[fruit] else {
+            return
+        }
+        self.fruitStock.updateValue(currentStock + count, forKey: fruit)
+    }
+    
+    func decreasStock(of fruit: Fruit, count: Int) {
+        guard let currentStock = fruitStock[fruit], currentStock > .zero else {
+            return
+        }
+        self.fruitStock.updateValue(currentStock - count, forKey: fruit)
+    }
+}
