4c0a6d39ed7015ea44d6f4543d6dfedd175c7fba
horeng2 <huski.a.a@gmail.com>
2022-05-25 19:05:08 +0900

    feat: EditViewModel 타입 구현

--- JuiceMaker/JuiceMaker/ViewModel/EditViewModel.swift
+++ JuiceMaker/JuiceMaker/ViewModel/EditViewModel.swift
@@ -9,5 +9,43 @@ import Foundation
 import RxSwift
 
 class EditViewModel {
+    private let juiceMaker = JuiceMaker()
+    private let disposeBag = DisposeBag()
+    
+    struct Input {
+        let stockChange: PublishSubject<(Fruit, StockChangeOperator)>
+    }
+    
+    struct Output {
+        let modifyStockSuccess: PublishSubject<Bool>
+    }
+    
+    func transform(input: Input) -> Output {
+        let modifyStockSuccess = PublishSubject<Bool>()
+        
+        input.stockChange
+            .map{ changing in
+                self.juiceMaker.modifyFruitStock(changing.1, for: changing.0)
+            }.subscribe(onNext: { modifyResult in
+                modifyResult
+                    .subscribe(onNext: { result in
+                        if result == true {
+                            modifyStockSuccess.onNext(true)
+                        } else {
+                            modifyStockSuccess.onNext(false)
+                        }
+                    }).disposed(by: self.disposeBag)
+            }).disposed(by: disposeBag)
+        
+        return Output(modifyStockSuccess: modifyStockSuccess)
+    }
+    
+    func fruitStockObservable(of fruit: Fruit) -> Observable<Int> {
+        return juiceMaker.fruitStockObservable(of: fruit)
+    }
+}
 
+enum StockChangeOperator {
+    case up
+    case down
 }
