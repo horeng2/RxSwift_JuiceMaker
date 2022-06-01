294c792adfb3d6e4c2e2b4db02fd9ac887fba983
horeng2 <huski.a.a@gmail.com>
2022-06-01 20:08:08 +0900

    refactor: fruitStock을 dictionary에서 Observable로 리팩토링 #6

--- JuiceMaker/JuiceMaker/Repository/FruitRepository.swift
+++ JuiceMaker/JuiceMaker/Repository/FruitRepository.swift
@@ -10,33 +10,44 @@ import RxSwift
 
 class FruitRepository {
     static let shared = FruitRepository()
-    private var fruitStock = [Fruit : Int]()
-    private let initialStock = 10
+    private var fruitStock: Observable<[Fruit : Int]>
+    static let initialStock = 10
     static let maximumStock = 100
     
     init() {
+        var initialFruitStocks = [Fruit : Int]()
         Fruit.allCases.forEach { fruit in
-            self.fruitStock.updateValue(self.initialStock, forKey: fruit)
+            initialFruitStocks.updateValue(FruitRepository.initialStock, forKey: fruit)
         }
+        self.fruitStock = Observable.just(initialFruitStocks)
     }
     
     func readStock(of fruit: Fruit) -> Observable<Int> {
-        return Observable.just(self.fruitStock[fruit])
-        .compactMap{ $0 }
+        return self.fruitStock
+            .flatMap{ stocks in
+            Observable.just(stocks[fruit])
+                .compactMap{ $0 }
+        }
     }
     
     func updateStock(of fruit: Fruit, to newQuantity: Int) -> Observable<Bool> {
-        guard fruitStock[fruit] != nil else {
-            return Observable.just(false)
+        let newStocks = self.fruitStock.map{ stocks -> [Fruit: Int] in
+            var newStocks = stocks
+            newStocks.updateValue(newQuantity, forKey: fruit)
+            return newStocks
         }
-        self.fruitStock.updateValue(newQuantity, forKey: fruit)
+        self.fruitStock = newStocks
+        
         return Observable.just(true)
     }
     
     func decreaseStock(of fruit: Fruit, count: Int) {
-        guard let currentStock = fruitStock[fruit], currentStock > .zero else {
-            return
+        self.readStock(of: fruit).map{ currentStock in
+            guard currentStock == .zero else {
+                return
+            }
+            self.updateStock(of: fruit, to: currentStock - count)
         }
-        self.fruitStock.updateValue(currentStock - count, forKey: fruit)
+      
     }
 }
